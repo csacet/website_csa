50d0b64bd9ece42c1be46cdd0b0328b466bbc845
Riya Sabu <riyasabures@gmail.com>
2024-10-19 20:43:51 +0530

    pages added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        primary: ['Bricolage Grotesque', 'sans-serif'],
+        secondary: ['Bruno Ace SC', 'cursive'],
+      },
+      fontSize: {
+        base: ['20px', '24px'], // 20px font with 24px line height
+        large: ['80px', '96.48px'], // 80px font with 96.48px line height
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
