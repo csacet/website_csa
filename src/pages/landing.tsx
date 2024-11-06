e44f74318275af1d95a228b46cf387eca8000a59
Aharon Mathews <aharonmathews04@gmail.com>
2024-11-06 08:04:23 +0530

    Landing page updated

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,39 +5,87 @@ import sampleDepartmentImage from '../app/assets/images/sampleDepartmentImage.sv
 import landingImg1 from '../app/assets/images/landingImg1.svg';
 import landingImg2 from '../app/assets/images/landingImg2.svg';
 import Footer from '../ui/Footer';
+import { useState, useEffect } from 'react';
 
-export default function landing() {
+export default function Landing() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [sampleDepartmentImage, landingImg1];
+
+    useEffect(() => {
+        const imageSwitchInterval = setInterval(() => {
+            setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
+        }, 2000);
+
+        return () => clearInterval(imageSwitchInterval);
+    }, []);
+
     return (
-
-        <div>
+        <div className="overflow-x-hidden">
             <Navbar />
-            {/* <div className='mx-[2rem] mt-[1.125rem] h-fit w-full relative text-center text-white brightness-50'>
-                <Image src={sampleDepartmentImage} alt='CS Department' className=' ' />
-                <div className='text-[5rem] absolute m-auto font-secondary items-center left-1/2 top-1/2'>CS ASSOCIATION</div>
-                <div className='text-[2rem] absolute m-auto'>Department of Computer Science</div>
-                <div className='border-[1px] rounded-[1.25rem] border-white absolute bottom-[2.125rem] left-[2.3125rem]'>
-                    <div className='text-[1.5rem] my-[1.25rem] mx-[2.1875rem]'>Contact Us</div>
+            <div className="relative mt-[1.125rem] mx-[2.03125rem] h-[400px] text-white">
+                <div className=" rounded-[1.25rem] overflow-hidden">
+                    <Image
+                        src={images[currentImage]}
+                        alt="CS Department"
+                        layout="fill"
+                        objectFit="cover"
+                        className="brightness-50"
+                    />
                 </div>
-                <div className='text-[1.5rem] absolute bottom-0 right-9 h-[8.5rem]'>College of Engineering Trivandrum</div>
-            </div> */}
-
-            <div className='mx-[2.03125rem] mt-[1.875rem] flex flex-row bg-[#F4F4F4] px-[4.875rem] py-[3.3125rem] rounded-[1.25rem] space-x-'>
-                <Image src={landingImg1} alt='Department of Computer Science' className=''/>
-                <div className='flex flex-col my-auto ml-[6.25rem]'>
-                    <div className='font-bold text-5xl'>Department of Computer Science</div>
-                    <div className='font-normal text-xl mt-[2.5rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
+                    <h1 className="text-4xl md:text-6xl font-secondary">CS ASSOCIATION</h1>
+                    <p className="text-lg md:text-2xl mt-1 md:mt-2">Department of Computer Science</p>
+                </div>
+                <div className="absolute bottom-10 left-5">
+                    <button className="border border-white rounded-lg px-8 py-3 text-lg">
+                        Contact Us
+                    </button>
+                </div>
+                <div className="absolute bottom-5 right-9 text-lg">
+                    College of Engineering Trivandrum
                 </div>
             </div>
 
-            <div className='mx-[2.03125rem] mt-[1.875rem] flex flex-row bg-[#F4F4F4] px-[4.875rem] py-[3.3125rem] rounded-[1.25rem] space-x-'>
-                
-                <div className='flex flex-col my-auto mr-[4.0625rem]'>
-                    <div className='font-bold text-5xl'>Department of Computer Science</div>
-                    <div className='font-normal text-xl mt-[2.5rem]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div className="mx-[2.03125rem] mt-[1.875rem] flex flex-col md:flex-row bg-[#F4F4F4] px-[2rem] md:px-[4.875rem] py-[2rem] md:py-[3.3125rem] rounded-[1.25rem] space-y-4 md:space-x-6">
+                <Image src={landingImg1} alt="Department of Computer Science" className="rounded-md w-full" />
+                <div className="flex flex-col my-auto ml-[6.25rem]">
+                    <h2 className="font-bold text-5xl">Department of Computer Science</h2>
+                    <p className="font-normal text-xl mt-[2.5rem]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
                 </div>
-                <Image src={landingImg2} alt='Department of Computer Science' className=''/>
+            </div>
+
+            <div className="mx-[2.03125rem] mt-[1.875rem] flex flex-col md:flex-row bg-[#F4F4F4] px-[2rem] md:px-[4.875rem] py-[2rem] md:py-[3.3125rem] rounded-[1.25rem] space-y-4 md:space-x-6">
+                <div className="flex flex-col my-auto mr-[4.0625rem]">
+                    <h2 className="font-bold text-5xl">College Of Engineering Trivandrum</h2>
+                    <p className="font-normal text-xl mt-[2.5rem]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <Image src={landingImg2} alt="Department of Computer Science" className="rounded-md" />
+            </div>
+
+            <div className="flex flex-col my-[1.875rem] items-center justify-center mx-[2.03125rem] bg-[#2C2C2C]  rounded-lg shadow-lg px-min">
+                <div className='px-[2.6875rem]'>
+                <h2 className="text-5xl font-bold text-white mb-4 mt-[2.625rem]">Do you have any queries?</h2>
+                <form className="flex w-full mb-[4.625rem] tems-center  mx-auto">
+                    <input
+                        type="text"
+                        placeholder="Write here..."
+                        className="px-4 py-2 mx-auto text-[#6A6A6A] rounded-[0.625rem] bg-[#424242] mr-4 w-[27.6875rem] focus:outline-none"
+                    />
+                    <button
+                        type="submit"
+                        className="px-[2.1875rem] py-[0.625rem] font-medium text-white bg-transparent border border-white rounded-[0.625rem] hover:bg-white hover:text-gray-800 transition duration-200"
+                    >
+                        Send
+                    </button>
+                </form>
+                </div>
+                
             </div>
             <Footer />
         </div>
-    )
+    );
 }
--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -1,23 +1,73 @@
-import csAssociationLogo from '../app/assets/images/csLogo.svg'
+import csAssociationLogo from '../app/assets/images/csLogo.svg';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 export default function Navbar() {
+    const [isSmallScreen, setIsSmallScreen] = useState(null);
+    const [menuActive, setMenuActive] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 1440);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
+
+    if (isSmallScreen === null) {
+        return null;
+    }
+
     return (
-        <div className="mx-[2rem] flex flex-row bg-[#1A1A1A] text-white text-xl rounded-b-[1.25rem] h-[6.375rem] items-center justify-between">
+        <div className="mx-[2rem] flex flex-col md:flex-row bg-[#1A1A1A] text-white text-xl rounded-b-[1.25rem] md:h-[6.375rem] h-fit p-5 items-center">
+            {!isSmallScreen && (
+                <div className="flex flex-row justify-between w-full">
+                    <div className="flex flex-row ml-[7.018125rem] space-x-[5rem]">
+                        <div className='hover:text-[#FFE53B]'>Achievements</div>
+                        <div className='hover:text-[#FFE53B]'>Events</div>
+                        <div className='hover:text-[#FFE53B]'>Newsletter</div>
+                    </div>
+                    <Image src={csAssociationLogo} alt="associationlogo" className="h-[4.0625rem]" />
+                    <div className="flex flex-row mr-[7.018125rem] space-x-[5rem]">
+                        <div className='hover:text-[#FFE53B]'>Dotslash</div>
+                        <div className='hover:text-[#FFE53B]'>CS-Cup</div>
+                        <div className='hover:text-[#FFE53B]'>Placements</div>
+                    </div>
+                </div>
+            )}
 
-            <div className='flex flex-row ml-[7.018125rem] space-x-[5rem]'>
-                <div>Achievements</div>
-                <div>Events</div>
-                <div>Newsletter</div>
-            </div>
-            <Image src={csAssociationLogo} alt='associationlogo' className='h-[4.0625rem]' />
-            <div className='flex flex-row mr-[7.018125rem] space-x-[5rem]'>
-                <div>Dotslash</div>
-                <div>CS-Cup</div>
-                <div>Placements</div>
-            </div>
+            {isSmallScreen && (
+                <div className="flex flex-row justify-between w-full">
+                    <div className="flex flex-row">
+                        <Image src={csAssociationLogo} alt="associationlogo" className="h-[3rem]" />
+                        <div className="text-lg text-center my-auto">Cs Association</div>
+                    </div>
+                    <div onClick={toggleMenu} className="mr-0 h-[2.5rem] w-[2.5rem] flex flex-col justify-center items-center cursor-pointer">
+                        <span className={`block w-[1.770625rem] h-[3px] bg-white transition-transform duration-300 ease-in-out ${menuActive ? 'transform translate-y-2 rotate-45 mb-[5px]' : 'mb-1 mt-[0.5rem]'}`}></span>
+                        <span className={`block w-[1.770625rem] h-[3px] bg-white transition-opacity duration-300 ease-in-out ${menuActive ? 'opacity-0 mb-[5px]' : 'mb-1'}`}></span>
+                        <span className={`block w-[1.770625rem] h-[3px] bg-white transition-transform duration-300 ease-in-out ${menuActive ? 'transform -translate-y-2 -rotate-45' : ''}`}></span>
+                    </div>
+                </div>
+            )}
             
-
+            {isSmallScreen && menuActive && (
+                <div className="bg-[#1A1A1A] rounded-[1.25rem] text-white flex flex-col space-y-3 z-50 p-4 ">
+                    <div className='ml-0'>Achievements</div>
+                    <div className='ml-0'>Events</div>
+                    <div className='ml-0'>Newsletter</div>
+                    <div className='ml-0'>Dotslash</div>
+                    <div className='ml-0'>CS-Cup</div>
+                    <div className='ml-0'>Placements</div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
